c97901488c044dca5e6b05238c0b2c81eba85617
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2019-04-03 10:49:37 +0700

    initial commit

---
+++ src/main/java/com/beehive/randang/RandangApplication.java
@@ -0,0 +1,13 @@
+package com.beehive.randang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandangApplication.class, args);
+	}
+
+}
