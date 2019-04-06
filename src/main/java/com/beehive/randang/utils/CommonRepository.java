6ebea4dc8624b5b3ee1d9d038b5b2b026a765589
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2019-04-07 01:07:18 +0700

    common repository is OK

--- src/main/java/com/beehive/randang/utils/CommonRepository.java
+++ src/main/java/com/beehive/randang/utils/CommonRepository.java
@@ -2,6 +2,8 @@ package com.beehive.randang.utils;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.NoRepositoryBean;
 
+@NoRepositoryBean
 public interface CommonRepository<T, U> extends JpaRepository<T, U>, JpaSpecificationExecutor<T> {
 }
